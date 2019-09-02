900a3295bba578f1ab835906d3e6bdc21654655d
vskus <vskus@softserveinc.com>
2019-09-02 10:46:34 +0300

    Added tests: LoginComponent, RegisterComponent, LoginService, RegisterService;

--- src/app/modules/login/login/login.component.spec.ts
+++ src/app/modules/login/login/login.component.spec.ts
@@ -1,25 +1,79 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
+import { LoginService } from "../login.service";
+import { Subject, of } from "rxjs";
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let loginServiceSpy = null;
+  let subscription: Subject<any>;
+
+  const user = {
+    login: 'login',
+    password: 'password'
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterTestingModule
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        {provide: 'LoginService', useValue: loginServiceSpy}
+        ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    loginServiceSpy = jasmine.createSpyObj('LoginService', ['login']);
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    loginServiceSpy.login.and.returnValue(of(user));
+    subscription = new Subject();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have login method', function() {
+    expect(loginServiceSpy.login).toBeDefined();
+  });
+
+  it('should invoke the method login', function() {
+    loginServiceSpy.login();
+    expect(loginServiceSpy.login).toHaveBeenCalled();
+  });
+
+  it('should accept user object', function() {
+    loginServiceSpy.login(user);
+    expect(loginServiceSpy.login).toHaveBeenCalledWith(user);
+  });
+
+  it('form should be invalid', function() {
+    component.getControl('login').setValue('');
+    component.getControl('password').setValue('');
+    expect(component.getControl('login').valid).toBeFalsy();
+    expect(component.getControl('password').valid).toBeFalsy();
+  });
+
+  it('form should be valid', function() {
+    component.getControl('login').setValue('user');
+    component.getControl('password').setValue('password');
+    expect(component.getControl('login').valid).toBeTruthy();
+    expect(component.getControl('password').valid).toBeTruthy();
   });
 });
--- src/app/modules/register/register/register.component.spec.ts
+++ src/app/modules/register/register/register.component.spec.ts
@@ -1,19 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterComponent } from './register.component';
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { User } from "../interfaces/user";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let regServiceSpy = null;
+  let user: User;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [RegisterComponent],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientModule
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        {provide: 'RegisterService', useValue: regServiceSpy}
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    regServiceSpy = jasmine.createSpyObj('RegisterService', ['register']);
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +35,45 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have register method', function() {
+    expect(regServiceSpy.register).toBeDefined();
+  });
+
+  it('should invoke the method register', function() {
+    regServiceSpy.register();
+    expect(regServiceSpy.register).toHaveBeenCalled();
+  });
+
+  it('should accept user object', function() {
+    regServiceSpy.register(user);
+    expect(regServiceSpy.register).toHaveBeenCalledWith(user);
+  });
+
+  it('form should be invalid', function() {
+    component.getControl('login').setValue('');
+    component.getControl('firstName').setValue('');
+    component.getControl('lastName').setValue('');
+    component.getControl('password').setValue('');
+
+    expect(component.getControl('login').valid).toBeFalsy();
+    expect(component.getControl('firstName').valid).toBeFalsy();
+    expect(component.getControl('lastName').valid).toBeFalsy();
+    expect(component.getControl('password').valid).toBeFalsy();
+  });
+
+  it('form should be valid', function() {
+    component.getControl('login').setValue('user@mail.com');
+    component.getControl('firstName').setValue('firstName');
+    component.getControl('lastName').setValue('lastName');
+    component.getControl('password').setValue('Mnj!13j69');
+    component.getControl('confirmPassword').setValue('Mnj!13j69');
+
+    expect(component.getControl('login').valid).toBeTruthy();
+    expect(component.getControl('firstName').valid).toBeTruthy();
+    expect(component.getControl('lastName').valid).toBeTruthy();
+    expect(component.getControl('password').valid).toBeTruthy();
+    expect(component.getControl('confirmPassword').valid).toBeTruthy();
   });
 });
